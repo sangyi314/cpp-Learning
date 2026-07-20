ed58bb8576db8f74df017b43a2269cd612a1a39f
sangyi314 <sangyi31415926@gmail.com>
2026-07-20 22:40:27 +0800

    cin-cout

---
+++ My-Learning-Journal/Projects/cin-cout.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Enter two numbers separated by a space"<<'\n';
+    int x{},y{};
+    std::cin>>x>>y;
+    std::cout<<"You entered "<<x<<" and "<<y;
+    return 0;
+}
