ec0c5818cb38e0dfcd8fcaf3c6064cdbd04b1b69
sangyi314 <sangyi31415926@gmail.com>
2026-07-23 16:56:34 +0800

    save008

--- My-Learning-Journal/Projects/two-functions-work-together1.cpp
+++ My-Learning-Journal/Projects/two-functions-work-together1.cpp
@@ -15,3 +15,6 @@ int main()
     std::cout << "The sum of " << x << " and " << y << " is" << add(x,y) << '\n';
     return 0;
 }
+
+//You need to run both of them once 
+//Like "g++ -std=c++23 -Wall -Wextra -pedantic two-functions-work-together1.cpp two-functions-work-together2.cpp -o two-functions-work-together"
