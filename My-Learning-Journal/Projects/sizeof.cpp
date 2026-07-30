0e086094f3249f791f62dfc17aa5c94a9139f3df
sangyi314 <sangyi31415926@gmail.com>
2026-07-30 18:14:19 +0800

    save009

---
+++ My-Learning-Journal/Projects/sizeof.cpp
@@ -0,0 +1,33 @@
+#include <iomanip>
+#include <iostream>
+#include <climits>
+
+int main()
+{
+    std::cout << "A byte is " << CHAR_BIT << " bits\n\n" ;
+    std::cout << std::left ;
+    std::cout << std::setw(16) << "bool:" << sizeof(bool) << " bytes\n" ;
+    std::cout << std::setw(16) << "char:" << sizeof(char) << " bytes\n" ;
+    std::cout << std::setw(16) << "short:" << sizeof(short) << " bytes\n" ;
+    std::cout << std::setw(16) << "int:" << sizeof(int) << " bytes\n" ;
+    std::cout << std::setw(16) << "long:" << sizeof(long) << " bytes\n" ;
+    std::cout << std::setw(16) << "long long:" << sizeof(long long) << " bytes\n" ;
+    std::cout << std::setw(16) << "float:" << sizeof(float) << " bytes\n" ;
+    std::cout << std::setw(16) << "double:" << sizeof(double) << " bytes\n" ;
+    std::cout << std::setw(16) << "long double:" << sizeof(long double) << " bytes\n" ;
+    return 0;
+}
+
+/*
+A byte is 8 bits
+
+bool:           1 bytes
+char:           1 bytes
+short:          2 bytes
+int:            4 bytes
+long:           8 bytes
+long long:      8 bytes
+float:          4 bytes
+double:         8 bytes
+long double:    16 bytes
+*/
