f29affe0bc0ff77d745ebe0dc93fef5c9d760a41
sangyi314 <sangyi31415926@gmail.com>
2026-08-16 11:47:40 +0800

    save016

---
+++ My-Learning-Journal/Projects/while.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+
+int main()
+{
+    int count{1};
+    while(count <= 10)
+    {
+        std::cout << count << ' ';
+        count++;
+    }
+    std::cout << "done!" << '\n';
+    return 0;
+
+}
