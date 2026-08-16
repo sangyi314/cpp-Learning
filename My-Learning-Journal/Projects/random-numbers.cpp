74cf83444158028f77828fa11ac4e6fe19745efb
sangyi314 <sangyi31415926@gmail.com>
2026-08-16 20:45:32 +0800

    save020

--- My-Learning-Journal/Projects/random-numbers.cpp
+++ My-Learning-Journal/Projects/random-numbers.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <random>
+#include <chrono>
 
 int main()
 {
-    std::mt19937 mt{};
+    // std::mt19937 mt{};
+    //std::mt19937 mt{ static_cast<std::mt19937::result_type>(std::chrono::steady_clock::now().time_since_epoch().count())};
+    std::mt19937 mt{std::random_device{}()};
     std::uniform_int_distribution die6{1,6};
     for (int count {1}; count <= 40 ; count++)
     {
