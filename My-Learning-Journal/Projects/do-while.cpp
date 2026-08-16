f0406bf128e3fd89321a562907c7544344ff1f5d
sangyi314 <sangyi31415926@gmail.com>
2026-08-16 12:12:46 +0800

    save017

---
+++ My-Learning-Journal/Projects/do-while.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    int selection {};
+    do
+    {
+        std::cout << "Please make a selection : \n";
+        std::cout << "1)Addition\n";
+        std::cout << "2)Subtraction\n";
+        std::cout << "3)Multiplication\n";
+        std::cout << "4)Division\n";
+        std::cin >> selection;
+    } while (selection < 1 || selection > 4);
+    
+    std::cout << "You selected option #" << selection << '\n';
+    return 0;
+}
