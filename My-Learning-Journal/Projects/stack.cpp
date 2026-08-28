d8839860c01e75f9c5ca99a45719230bcba03a65
sangyi314 <sangyi31415926@gmail.com>
2026-08-28 20:20:50 +0800

    save061

---
+++ My-Learning-Journal/Projects/stack.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+void printStack(const std::vector<int>& v)
+{
+    if (v.empty())
+        std::cout << "Empty";
+    for (int i : v)
+        std::cout << i << ' ';
+    std::cout << "\tLength: " << v.size() << "\tCapacity: " << v.capacity() << '\n';
+}
+
+int main()
+{
+    std::vector<int> stack {};
+
+    printStack(stack);
+
+    stack.push_back(1);
+    printStack(stack);
+
+    stack.push_back(2);
+    printStack(stack);
+
+    stack.push_back(3);
+    printStack(stack);
+
+    std::cout << "Top: " << stack.back() << '\n';
+
+    stack.pop_back();
+    printStack(stack);
+
+    stack.pop_back();
+    printStack(stack);
+
+    stack.pop_back();
+    printStack(stack);
+
+    return 0;
+}
