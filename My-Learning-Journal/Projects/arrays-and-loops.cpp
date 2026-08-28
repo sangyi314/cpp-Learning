dec58a1149ffc79e459e3c60d0a0c94141747b01
sangyi314 <sangyi31415926@gmail.com>
2026-08-28 14:03:13 +0800

    save057

--- My-Learning-Journal/Projects/arrays-and-loops.cpp
+++ My-Learning-Journal/Projects/arrays-and-loops.cpp
@@ -5,11 +5,9 @@ int main()
 {
     std::vector score{84,92,76,81,56};
     std::size_t length {score.size()};
-    int average {0};
-    for (std::size_t index{0}; index < length ;index++)
+    int average {0};e score vector.
     {
-        average += score[index];
-    }
+    for (std::size_t index{0}; index < length ;index++)  //The type std::size_t is an unsigned integral type.
     average /= length;
     std::cout << "The average socre is:" << average << '\n' ;
     return 0;
