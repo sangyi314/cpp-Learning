4b6848d817055bbaa7cb885ff07fcaebde9248bf
sangyi314 <sangyi31415926@gmail.com>
2026-09-01 08:34:55 +0800

    save066

---
+++ My-Learning-Journal/Projects/begin-end.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <array>
+
+int main ()
+{
+    std::array arr {1,2,3};
+    auto begin {arr.begin()};
+    auto end {arr.end()};
+    for (auto p{begin} ; p < end ; p++)
+        std::cout << *p <<  ' ';
+    std::cout << '\n';
+    return 0;
+}
