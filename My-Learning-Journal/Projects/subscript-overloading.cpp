9394363c67a2b54e5ca191464df1d5d9dd4c24e3
sangyi314 <sangyi31415926@gmail.com>
2026-09-03 18:40:45 +0800

    save073

---
+++ My-Learning-Journal/Projects/subscript-overloading.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <array>
+
+class IntArray
+{
+private :
+    int m_arr [5] {};
+public :
+    int& operator[](std::size_t index)
+    {
+        return m_arr[index];
+    }
+    const int& operator[](std::size_t index) const
+    {
+        return m_arr[index];
+    }
+};
+
+int main()
+{
+    IntArray arr ;
+    arr[0] = 5 ;
+    arr[2] = 4 ;
+    std::cout << arr[0] << '\n';
+}
